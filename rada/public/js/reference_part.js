44b670b116b24a00f86deb997ca225ad5311653c
Patrick Willy <46555287+pnwilly@users.noreply.github.com>
2025-07-18 11:08:59 +0300

    Fix year validation for Applicable Units

--- rada/public/js/reference_part.js
+++ rada/public/js/reference_part.js
@@ -1,9 +1,12 @@
-frappe.ui.form.on('Applicable Units', {
-    to_year: function(frm, cdt, cdn) {
-        const row = locals[cdt][cdn];
-        if (row.to_year && row.from_year && row.to_year < row.from_year) {
-            frappe.msgprint(__('To Year cannot be earlier than From Year'));
-            frappe.model.set_value(cdt, cdn, 'to_year', row.from_year);
-        }
+function validate_years(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
+    if (row.to_year && row.from_year && row.to_year < row.from_year) {
+        frappe.msgprint(__('To Year cannot be earlier than From Year'));
+        frappe.model.set_value(cdt, cdn, 'to_year', row.from_year);
     }
+}
+
+frappe.ui.form.on('Applicable Units', {
+    to_year: validate_years,
+    from_year: validate_years
 });
